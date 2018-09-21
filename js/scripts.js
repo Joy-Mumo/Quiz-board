1ab44d39a118f4278c0ad5e8b6dfda25cbe72136
Joy Vivika <joiemumo@gmail.com>
2018-09-21 16:03:47 +0300

    edit submit button color

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-var question=$("input:radio[name=Q1]:checked").val();
-var question=$("input:radio[name=Q2]:checked").val();
-var question=$("input:radio[name=Q3]:checked").val();
+var testResults = function(q1,q2,q3){
+return q1 + q2 + q3;
+
+
+}
+
+
+$().submit(function(event){
+    var q1 = parseInt($("input:radio[name=Q1]:checked").val());
+    var q2 = parseInt($("input:radio[name=Q2]:checked").val());
+    var q3 = parseInt($("input:radio[name=Q2]:checked").val());
+
+    var result = testResults(q1,q2,q3);
+
+    $("#display").text("Your score is:" + result);
+
+    event.preventDefault();
+  });
