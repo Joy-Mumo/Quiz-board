4a4bba93e927a863be77413e4e5e10c30851aa8c
Joy Vivika <joiemumo@gmail.com>
2018-09-22 22:28:59 +0300

    add result var

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-var add = function(q1,q2,q3){
-return  q1 + q2 + q3;
+function check() {
+  var q1 =parseInt( $("input:radio[name=Q1]:checked").val());
+  var q2 = parseInt($("input:radio[name=Q2]:checked").val());
+  var q3 = parseInt($("input:radio[name=Q2]:checked").val());
+
+  var testResults = function(q1,q2,q3){
+return (q1+ q2 + q3);
+}
+
+var result = testResults(q1, q2 ,q3);
+
+
+  document.getElementById("display").style.visibility="visible";
+  document.getElementById("number correct").innerHTML=" You got " +  result;
 
 
 }
-
-$("#radio").submit(function(event){
-    var q1 = parseInt($("input:radio[name=Q1]:checked").val());
-    var q2 = parseInt($("input:radio[name=Q2]:checked").val());
-    var q3 = parseInt($("input:radio[name=Q2]:checked").val());
-
-    var result = add(q1,q2,q3);
-
-    $("#display").text( result);
-
-    event.preventDefault();
-  });
