39af1a4e1b4af9506633e785b853956cdeae6afc
Joy Vivika <joiemumo@gmail.com>
2018-09-23 16:22:07 +0300

    debug html

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 function check() {
-  var q1 =parseInt( $("input:radio[name=Q1]:checked").val());
-  var q2 = parseInt($("input:radio[name=Q2]:checked").val());
-  var q3 = parseInt($("input:radio[name=Q2]:checked").val());
+  var q1 = parseInt($("input:radio[name=q1]:checked").val());
+  var q2 = parseInt($("input:radio[name=q2]:checked").val());
+  var q3 = parseInt($("input:radio[name=q3]:checked").val());
 
   var testResults = function(q1,q2,q3){
-return (q1+ q2 + q3);
+return (q1+ q2 + q3 );
 }
 
-var result = testResults(q1, q2 ,q3);
+var result = testResults(q1, q2 , q3,);
 
 
   document.getElementById("display").style.visibility="visible";
-  document.getElementById("number correct").innerHTML=" You got " +  result;
+  document.getElementById("answer").innerHTML=" You got " +  result + " result. ";
 
 
 }
